df0fdc4cc5590f621a5467e0d1aa39f7b336c0cc
Linh On <lmon@uci.edu>
2026-06-09 02:41:54 -0700

    Fix the home page layout

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,23 +1,23 @@
 interface Language {
-    name: string;
-    level: string;
-    description: string;
-    show: boolean;
+  name: string;
+  level: string;
+  description: string;
+  show: boolean;
 }
 
 const languages: Language[] = [
-    {
-        name: "English",
-        level: "Native/Fluent",
-        description: "Native speaker — fluent in reading, writing, and speaking",
-        show: true
-    },
-    {
-        name: "Vietnamese",
-        level: "Fluent",
-        description: "Fluent in speaking and writing",
-        show: true
-    }
+  {
+    name: "English",
+    level: "Fluent",
+    description: "Native speaker — fluent in reading, writing, and speaking",
+    show: true,
+  },
+  {
+    name: "Vietnamese",
+    level: "Native/Fluent",
+    description: "Fluent in speaking and writing",
+    show: true,
+  },
 ];
 
 export default languages;
--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,9 +1,10 @@
 export const menu = [
-    { href: '/', label: 'Home' },
-    { href: '/#about', label: 'About' },
-    { href: '/projects', label: 'Projects' },
-    { href: '/#experience', label: 'Experience' },
-    { href: '/#skills', label: 'Skills' },
-    { href: '/contact', label: 'Contact' },
-    { href: '/blog', label: 'Blog' },
-]
+  { href: "/", label: "Home" },
+  { href: "/#about", label: "About" },
+  { href: "/#projects", label: "Projects" },
+  { href: "/#experience", label: "Experience" },
+
+  { href: "/#skills", label: "Skills" },
+  { href: "/contact", label: "Contact" },
+  { href: "/blog", label: "Blog" },
+];
